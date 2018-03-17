3bb5f33b08a27d67491ddfb3e6bc369a18455f21
Jan W <jan.weidenhaupt@gmx.de>
2018-03-17 10:44:03 +0100

    started refactoring

--- senv/main.go
+++ senv/main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/jamowei/senv"
 	"github.com/spf13/cobra"
 	"os"
 	"os/exec"
-	"bytes"
 	"syscall"
 )
 
@@ -18,8 +18,8 @@ var version = "0.0.0"
 var date = "2018"
 
 var (
-	host, port, name, label          string
-	profiles                         []string
+	host, port, name, label, command string
+	profiles, files                  []string
 	noSysEnv, json, verbose, content bool
 )
 
@@ -33,8 +33,21 @@ func main() {
 	}
 }
 
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&noSysEnv, "nosysenv", "s", false, "start without system-environment variables")
+	rootCmd.PersistentFlags().BoolVarP(&json, "json", "j", false, "print json to stdout")
+	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose")
+	rootCmd.PersistentFlags().StringVar(&host, "host", hostDefault, "config-server host")
+	rootCmd.PersistentFlags().StringVar(&port, "port", portDefault, "config-server port")
+	rootCmd.PersistentFlags().StringVarP(&name, "name", "n", nameDefault, "spring.application.name")
+	rootCmd.PersistentFlags().StringSliceVarP(&profiles, "profiles", "p", profileDefault, "spring.active.profiles")
+	rootCmd.PersistentFlags().StringSliceVarP(&files, "files", "f", []string{}, "files to fetch from config-server")
+	rootCmd.PersistentFlags().StringVarP(&label, "label", "l", labelDefault, "config-repo label to be used")
+	rootCmd.PersistentFlags().StringVarP(&command, "command", "c", "", "command line to execute")
+}
+
 var rootCmd = &cobra.Command{
-	Use:     "senv [command]",
+	Use:     "senv",
 	Short:   "A native config-client for the spring-cloud-config-server",
 	Version: "v" + version,
 	Long: fmt.Sprintf(
@@ -42,70 +55,101 @@ var rootCmd = &cobra.Command{
 
 Senv is a fast native config-client for a
 spring-cloud-config-server written in Go`, version, date[:4]),
-	Args: cobra.NoArgs,
+	Args:    cobra.NoArgs,
+	PreRun:  warningDefault,
+	PreRunE: validateArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg := senv.NewConfig(host, port, name, profiles, label)
+		if len(files) > 0 {
+			if err := fetchFiles(files); err != nil {
+				return err
+			}
+		}
+		if command != "" {
+			if err := cfg.Fetch(json, verbose); err != nil {
+				return err
+			}
+			if err := cfg.Process(); err != nil {
+				return err
+			}
+			return runCommand(command, cfg.Properties, verbose, noSysEnv)
+		}
+		return nil
+	},
+}
+
+func validateArgs(_ *cobra.Command, _ []string) error {
+	if command == "" && len(files) == 0 {
+		return errors.New("no files or command given")
+	}
+	return nil
 }
 
 func warningDefault(_ *cobra.Command, _ []string) {
 	if name == nameDefault {
-		fmt.Fprintln(os.Stderr, "warning: no application name given, using default 'application'")
+		fmt.Println("warning: no application name given, using default 'application'")
 	}
 }
 
-var envCmd = &cobra.Command{
-	Use:   "env [command]",
-	Short: "Fetches properties and sets them as environment variables",
-	Long: `Fetches properties from the spring-cloud-config-server
-and replaces the placeholder in the specified command.`,
-	Example: `on config-server:
-  spring.application.name="Senv"
-Example call:
-  senv env echo ${spring.application.name:default}  //prints 'Senv' or when config-server not reachable 'default'`,
-	PreRun:       warningDefault,
-	SilenceUsage: true,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		cfg := senv.NewConfig(host, port, name, profiles, label)
-		if err := cfg.Fetch(json, verbose); err != nil {
-			return err
-		}
-		if err := cfg.Process(); err != nil {
-			return err
-		}
-		return runCommand(args, cfg.Properties, noSysEnv)
-	},
-}
+//var envCmd = &cobra.Command{
+//	Use:   "env [command]",
+//	Short: "Fetches properties and sets them as environment variables",
+//	Long: `Fetches properties from the spring-cloud-config-server
+//and replaces the placeholder in the specified command.`,
+//	Example: `on config-server:
+//  spring.application.name="Senv"
+//Example call:
+//  senv env echo ${spring.application.name:default}  //prints 'Senv' or when config-server not reachable 'default'`,
+//	PreRun:       warningDefault,
+//	SilenceUsage: true,
+//	RunE: func(cmd *cobra.Command, args []string) error {
+//		cfg := senv.NewConfig(host, port, name, profiles, label)
+//		if err := cfg.Fetch(json, verbose); err != nil {
+//			return err
+//		}
+//		if err := cfg.Process(); err != nil {
+//			return err
+//		}
+//		return runCommand(args, cfg.Properties, noSysEnv)
+//	},
+//}
 
-func runCommand(args []string, props map[string]string, noSysEnv bool) error {
+func runCommand(command string, props map[string]string, verbose bool, noSysEnv bool) error {
 	repl := senv.SpringReplacer{Opener: "${", Closer: "}", Default: ":"}
-	for i, arg := range args {
-		if val, err := repl.Replace(arg, props); err == nil {
-			args[i] = val
-		}
+	commandRepl, err := repl.Replace(command, props)
+	if err != nil {
+		return err
 	}
-	cmd := exec.Command(args[0], args[1:]...)
+
+	if verbose {
+		fmt.Println("executing:", commandRepl)
+	}
+
+	cmd := exec.Command(commandRepl)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	if dir, err := os.Getwd(); err != nil {
+		cmd.Dir = dir
+	}
+
 	if !noSysEnv {
 		cmd.Env = os.Environ()
 	}
-	var buffSout, buffSerr bytes.Buffer
-	cmd.Stdout = &buffSout
-	cmd.Stderr = &buffSerr
-	err := cmd.Run()
+	//var buffSout, buffSerr bytes.Buffer
+	//cmd.Stdout = &buffSout
+	//cmd.Stderr = &buffSerr
+	//
+	//serr := buffSerr.String()
+	//sout := buffSout.String()
 
-	serr := buffSerr.String()
-	sout := buffSout.String()
-
+	err = cmd.Run()
 	if err != nil {
 		// try to get the exit code
 		if exitError, ok := err.(*exec.ExitError); ok {
 			ws := exitError.Sys().(syscall.WaitStatus)
 			errExitCode = ws.ExitStatus()
-		} else {
-			// This will happen (in OSX) if `name` is not available in $PATH,
-			// in this situation, exit code could not be get, and stderr will be
-			// empty string very likely, so we use the default fail code, and format err
-			// to string and set to stderr
-			if serr == "" {
-				serr = err.Error()
-			}
 		}
 	} else {
 		// success, errExitCode should be 0 if go is ok
@@ -113,49 +157,27 @@ func runCommand(args []string, props map[string]string, noSysEnv bool) error {
 		errExitCode = ws.ExitStatus()
 	}
 
-	fmt.Fprintln(os.Stderr, serr)
-	fmt.Fprintln(os.Stdout, sout)
+	//fmt.Fprintln(os.Stderr, serr)
+	//fmt.Fprintln(os.Stdout, sout)
 
-	return nil
+	return err
 }
 
-var fileCmd = &cobra.Command{
-	Use:          "file [filenames]",
-	Short:        "Receives static file(s)",
-	Long:         `Receives static file(s) from the spring-cloud-config-server`,
-	Args:         cobra.MinimumNArgs(1),
-	PreRun:       warningDefault,
-	SilenceUsage: true,
-	RunE: func(cmd *cobra.Command, args []string) error {
-		cfg := senv.NewConfig(host, port, name, profiles, label)
-		if len(args) == 1 {
-			return cfg.FetchFile(args[0], content, verbose)
-		} else if len(args) > 1 {
-			isErr := false
-			for i, file := range args {
-				if err := cfg.FetchFile(file, content, verbose); err != nil {
-					isErr = true
-					fmt.Fprintf(os.Stderr, "error receiving %v. file %s: %s\n", i+1, file, err)
-				}
-			}
-			if isErr {
-				return fmt.Errorf("error while receiving files")
+func fetchFiles(files []string) error {
+	cfg := senv.NewConfig(host, port, name, profiles, label)
+	if len(files) == 1 {
+		return cfg.FetchFile(files[0], content, verbose)
+	} else if len(files) > 1 {
+		isErr := false
+		for i, file := range files {
+			if err := cfg.FetchFile(file, content, verbose); err != nil {
+				isErr = true
+				fmt.Fprintf(os.Stderr, "error receiving %v. file %s: %s\n", i+1, file, err)
 			}
 		}
-		return nil
-	},
-}
-
-func init() {
-	envCmd.PersistentFlags().BoolVarP(&noSysEnv, "nosysenv", "s", false, "start without system-environment variables")
-	envCmd.PersistentFlags().BoolVarP(&json, "json", "j", false, "print json to stdout")
-	envCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "verbose")
-	fileCmd.PersistentFlags().BoolVarP(&content, "content", "c", false, "print file to stdout")
-	rootCmd.PersistentFlags().StringVar(&host, "host", hostDefault, "configserver host")
-	rootCmd.PersistentFlags().StringVar(&port, "port", portDefault, "configserver port")
-	rootCmd.PersistentFlags().StringVarP(&name, "name", "n", nameDefault, "spring.application.name")
-	rootCmd.PersistentFlags().StringSliceVarP(&profiles, "profiles", "p", profileDefault, "spring.active.profiles")
-	rootCmd.PersistentFlags().StringVarP(&label, "label", "l", labelDefault, "config-repo label to be used")
-	rootCmd.AddCommand(envCmd)
-	rootCmd.AddCommand(fileCmd)
+		if isErr {
+			return fmt.Errorf("error while receiving files")
+		}
+	}
+	return nil
 }
